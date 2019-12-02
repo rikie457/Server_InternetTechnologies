5af5e97f9b71f9d8a16ae682f803209ddb8b4c65
rikie <rikie456@gmail.com>
2019-12-02 23:38:31 +0100

    First things

--- src/nl/MenTych/Main.java
+++ src/nl/MenTych/Main.java
@@ -3,7 +3,6 @@ package nl.MenTych;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
         Thread server = new Thread(new Server());
         server.start();
     }
